ac10b9b6f743440921565362120b7d1ffeade826
anhnguyen <nguyenlamanh1701@gmail.com>
2022-07-24 17:52:20 +0700

    ud

--- Java he/Thread Netbean/Thread/src/Bai6.java
+++ Java he/Thread Netbean/Thread/src/Bai6.java
@@ -1,41 +1,44 @@
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author nvcc
  */
 public class Bai6 {
 
-    public static void main(String[] args) throws ParseException {
-        Scanner sc = new Scanner(System.in);
-        String time = sc.nextLine();
-
-        SimpleDateFormat f = new SimpleDateFormat("hh:mm:ss");
-        long targetTime = f.parse(time).getHours() * 60 * 60 + f.parse(time).getMinutes() * 60 * 60 + f.parse(time).getSeconds();
-//        System.out.println(f.parse(time).getHours() + " " + f.parse(time).getMinutes() + " " + f.parse(time).getSeconds());
+    public static void main(String[] args) {
 
         new Thread() {
+            long currTime;
+            long targetTime = System.currentTimeMillis() + 10000;
+
             public void run() {
                 while (true) {
-                    Date d = new Date();
-                    long currTime = d.getHours() * 60 * 60 + d.getMinutes() * 60 * 60 + d.getSeconds();
-//                    System.out.println(d.getHours() + " " + d.getMinutes() + " " + d.getSeconds());
-//                    System.out.println(currTime + " " + targetTime);
+                    currTime = System.currentTimeMillis();
                     if (currTime >= targetTime) {
-                        System.out.println("****** RENG RENG RENG *******");
                         break;
                     }
+
+                    System.out.println("cuccu");
+
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Bai6.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
+                System.out.println("Cuccu Cucu Cucu");
             }
         }.start();
-    }
 
+    }
 }
