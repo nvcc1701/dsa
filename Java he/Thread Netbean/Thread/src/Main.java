e16db4163ff7c7a31509a133a7bdd23c56995f41
anhnguyen <nguyenlamanh1701@gmail.com>
2022-07-24 16:58:28 +0700

    bai4 chu chay

---
+++ Java he/Thread Netbean/Thread/src/Main.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author nvcc
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ChuChay c = new ChuChay();
+
+        Thread t = new Thread(c);
+        t.start();
+
+        Scanner sc = new Scanner(System.in);
+        String in = null;
+
+        while (sc.hasNext() && !(in = sc.next()).equalsIgnoreCase("exit")) {
+            if (in.equalsIgnoreCase("c")) {
+                t.suspend();
+            } else {
+                t.resume();
+            }
+        }
+
+        t.stop();
+        sc.close();
+    }
+}
