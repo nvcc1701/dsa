0885d2d99e32325c212698e2a3f053a2294e3f30
anh nguyen <nguyenlamanh1701@gmail.com>
2023-03-21 21:25:52 +0700

    +1

--- dsa_a_loc/contest14_phuong_phap_sinh/bai30_balo_0_1.cpp
+++ dsa_a_loc/contest14_phuong_phap_sinh/bai30_balo_0_1.cpp
@@ -10,11 +10,56 @@ const int INF = 1e9;
 const int MOD = 1e9 + 7;
 const int MAX = 1e6 + 5;
 
+int w[16];
+int v[16];
+int a[16];
+int n;
+int s;
+ll res = -INF;
+
+ll value()
+{
+    ll ans = 0;
+    ll weight = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (a[i])
+        {
+            weight += w[i];
+            if (weight > s)
+                return -1;
+            ans += v[i];
+        }
+    }
+    return ans;
+}
+
+void Try(int i)
+{
+    for (int j = 0; j <= 1; j++)
+    {
+        a[i] = j;
+        if (i == n)
+        {
+            res = max(res, value());
+        }
+        else
+        {
+            Try(i + 1);
+        }
+    }
+}
+
 void run_case()
 {
+    cin >> n >> s;
+    for (int i = 1; i <= n; i++)
+        cin >> w[i];
+    for (int i = 1; i <= n; i++)
+        cin >> v[i];
 
-
-
+    Try(1);
+    cout << res;
 }
 
 int main()
