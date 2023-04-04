7715623b98e54cb2db5823ffae34bdf53a47beb9
anhnguyen1701 <nguyenlamanh1701@gmail.com>
2023-04-05 00:40:42 +0700

    +9

--- PTIT/CTLDGT_Hoc_L2/contest6_sap_xep_tim_kiem/sap_xep_day_con_lien_tuc.cpp
+++ PTIT/CTLDGT_Hoc_L2/contest6_sap_xep_tim_kiem/sap_xep_day_con_lien_tuc.cpp
@@ -10,27 +10,89 @@ const int INF = 1e9;
 const int MOD = 1e9 + 7;
 const int MAX = 1e6 + 5;
 
+int n;
 void run_case()
 {
-    int n;
     cin >> n;
     int a[n];
-    for (auto &x : a)
-        cin >> x;
-
-    int cnt = 0;
+    int b[n];
     for (int i = 0; i < n; i++)
     {
-        if (a[i] > a[i - 1])
-            cnt++;
-        
+        cin >> a[i];
+        b[i] = a[i];
     }
+
+    sort(a, a + n);
+
+    int start, end;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != b[i])
+        {
+            start = i + 1;
+            break;
+        }
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (a[i] != b[i])
+        {
+            end = i + 1;
+            break;
+        }
+    }
+
+    cout << start << " " << end << "\n";
+
+    // bool first = false;
+    // bool ok = false;
+    // int start, end;
+
+    // for (int i = 1; i < n - 1; i++)
+    // {
+    //     if(ok) break;
+
+    //     if ((a[i - 1] > a[i]) && !first)
+    //     {
+    //         start = i;
+    //         while (true)
+    //         {
+    //             if (a[i] > a[start])
+    //             {
+    //                 start++;
+    //                 first = true;
+    //                 break;
+    //             }
+    //             start--;
+    //         }
+    //     }
+
+    //     if (a[i] > a[i + 1] && first)
+    //     {
+    //         end = i;
+    //         while (true)
+    //         {
+    //             if (a[end] < a[i])
+    //             {
+    //                 end = i - 1;
+    //                 ok = true;
+    //                 break;
+    //             }
+    //             i++;
+    //         }
+    //     }
+    // }
+    // // 1 2 4 3 7 5 6
+    // // + + + - + - +
+
+    // cout << start + 1 << " " << end + 1 << endl;
 }
 
 int main()
 {
 
-#define LOCAL
+// #define LOCAL
 
 #ifdef LOCAL
     freopen("../input.txt ", "r", stdin);
@@ -42,7 +104,7 @@ int main()
     cin.tie(nullptr);
 
     int Test = 1;
-    // cin >> Test;
+    cin >> Test;
     for (int test = 1; test <= Test; test++)
     {
         run_case();
---
+++ PTIT/CTLDGT_Hoc_L2/contest6_sap_xep_tim_kiem/sap_xep_so.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define endl '\n';
+
+const ll LINF = 1e18 + 5;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int MAX = 1e6 + 5;
+
+void run_case()
+{
+    set<char> se;
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    string s;
+    getline(cin, s);
+
+    for (auto x : s)
+    {
+        if (isdigit(x))
+            se.insert(x);
+    }
+
+    for (auto x : se)
+        cout << x << " ";
+    cout << endl;
+}
+
+int main()
+{
+
+// #define LOCAL
+
+#ifdef LOCAL
+    freopen("../input.txt ", "r", stdin);
+    freopen("../output.txt ", "w", stdout);
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Test = 1;
+    cin >> Test;
+    for (int test = 1; test <= Test; test++)
+    {
+        run_case();
+    }
+
+#ifdef LOCAL
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto startT = chrono::system_clock::to_time_t(start);
+    cout << "\n\n" << ctime(&startT) << (double)duration.count() / 1000 << " seconds\n";
+#endif
+
+    return 0;
+}
