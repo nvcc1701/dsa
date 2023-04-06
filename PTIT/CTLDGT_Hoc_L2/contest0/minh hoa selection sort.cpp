0fed602357c2203f4f2311b09fa88bdc31c2575b
anhnguyen1701 <nguyenlamanh1701@gmail.com>
2023-04-07 00:28:25 +0700

    update

---
+++ PTIT/CTLDGT_Hoc_L2/contest0/minh hoa selection sort.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define endl '\n';
+
+const ll LINF = 1e18 + 5;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int MAX = 1e6 + 5;
+
+int n;
+vector<int> v;
+
+void selection_sort()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[min_index])
+                min_index = j;
+        }
+
+        swap(v[min_index], v[i]);
+    }
+}
+
+void run_case()
+{
+    // nhap
+    cin >> n;
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+
+    selection_sort();
+
+    for (auto x : v)
+        cout << x << " ";
+}
+
+int main()
+{
+
+#define LOCAL
+
+#ifdef LOCAL
+    freopen("../input.txt ", "r", stdin);
+    freopen("../output.txt ", "w", stdout);
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Test = 1;
+    // cin >> Test;
+    for (int test = 1; test <= Test; test++)
+    {
+        run_case();
+    }
+
+#ifdef LOCAL
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto startT = chrono::system_clock::to_time_t(start);
+    cout << "\n\n" << ctime(&startT) << (double)duration.count() / 1000 << " seconds\n";
+#endif
+
+    return 0;
+}
