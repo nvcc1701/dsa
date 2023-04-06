2a0704a835ef8bee5e02528ae9a3257fe193474b
anhnguyen1701 <nguyenlamanh1701@gmail.com>
2023-04-06 12:00:08 +0700

    update

---
+++ PTIT/CTLDGT_Hoc_L2/zbt/phan_tich_thua_so_nguyen_to.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define endl '\n';
+
+const ll LINF = 1e18 + 5;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int MAX = 1e6 + 5;
+
+int n;
+vector<pair<int, int>> v;
+
+void factorize()
+{
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            int cnt = 0;
+            while (n % i == 0)
+            {
+                n /= i;
+                cnt++;
+            }
+            v.push_back({i, cnt});
+        }
+    }
+
+    if (n != 1)
+        v.push_back({n, 1});
+}
+
+void run_case()
+{
+    cin >> n;
+    factorize();
+
+    for (auto x : v)
+    {
+        cout << x.first << "(" << x.second << ") ";
+    }
+    cout << endl;
+
+    v.clear();
+}
+
+int main()
+{
+
+#define LOCAL
+
+#ifdef LOCAL
+    freopen("../input.txt ", "r", stdin);
+    freopen("../output.txt ", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Test = 1;
+    cin >> Test;
+    for (int test = 1; test <= Test; test++)
+    {
+        cout << "Test " << test << ": ";
+        run_case();
+    }
+
+    return 0;
+}
---
+++ PTIT/CTLDGT_Hoc_L2/zbt/uoc_chung_lon_nhat_boi_chung_lon_nhat.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define endl '\n';
+
+const ll LINF = 1e18 + 5;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int MAX = 1e6 + 5;
+
+// int gcd(int a, int b)
+// {
+//     if (b == 0)
+//         return a;
+//     else
+//         return gcd(b, a % b);
+// }
+
+int gcd(int a, int b)
+{
+    for (int i = max(a, b); i >= 0; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
+}
+
+void run_case()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << " " << (a * b) / gcd(a, b) << endl;
+}
+
+int main()
+{
+
+#define LOCAL
+
+#ifdef LOCAL
+    freopen("../input.txt ", "r", stdin);
+    freopen("../output.txt ", "w", stdout);
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Test = 1;
+    cin >> Test;
+    for (int test = 1; test <= Test; test++)
+    {
+        run_case();
+    }
+
+#ifdef LOCAL
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto startT = chrono::system_clock::to_time_t(start);
+    cout << "\n\n" << ctime(&startT) << (double)duration.count() / 1000 << " seconds\n";
+#endif
+
+    return 0;
+}
